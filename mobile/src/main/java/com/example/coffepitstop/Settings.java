4fee0afdf8eca39cf6e7890213682640bc92f6f6
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2019-05-26 18:59:35 +0200

    NFC quite there

--- mobile/src/main/java/com/example/coffepitstop/Settings.java
+++ mobile/src/main/java/com/example/coffepitstop/Settings.java
@@ -24,7 +24,6 @@ import static android.nfc.NdefRecord.createMime;
 public class Settings extends AppCompatActivity implements CreateNdefMessageCallback {
     private TextView textViewS;
     private NfcAdapter nfcAdapter;
-    private String beamTopicArn;
 
 
 
@@ -78,38 +77,15 @@ public class Settings extends AppCompatActivity implements CreateNdefMessageCall
 
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
-        String msgToBeam = Util.getSharedPreferences("topicArn", getApplicationContext());
+
+        Log.d("NFC", "Message created.");
+        String msgToBeam = Util.getSharedPreferences("topicName", getApplicationContext());
 
         NdefMessage msg = new NdefMessage( new NdefRecord[] { createMime( "application/vnd.com.example.android.beam", msgToBeam.getBytes())});
 
         return msg;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Check to see that the Activity started due to an Android Beam
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }
-    }
-
-    void processIntent(Intent intent) {
-        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        // only one message sent during the beam
-        NdefMessage msg = (NdefMessage) rawMsgs[0];
-        // record 0 contains the MIME type, record 1 is the AAR, if present
-
-        beamTopicArn = new String(msg.getRecords()[0].getPayload());
-
-        Toast.makeText(getApplicationContext(),beamTopicArn,Toast.LENGTH_SHORT);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        // onResume gets called after this to handle the intent
-        setIntent(intent);
-    }
 
 
 
--- mobile/src/main/java/com/example/coffepitstop/TopicsSubscriptions.java
+++ mobile/src/main/java/com/example/coffepitstop/TopicsSubscriptions.java
@@ -1,12 +1,16 @@
 package com.example.coffepitstop;
 
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.SubscribeRequest;
@@ -90,5 +94,42 @@ public class TopicsSubscriptions extends AppCompatActivity {
 
             return false;
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Log.d("AOOO", "Connection received.");
+            processIntent(getIntent());
+        }
+    }
+
+    void processIntent(Intent intent) {
+        Log.d("AOOO", "Process intent received.");
+
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+
+        topicName = new String(msg.getRecords()[0].getPayload());
+        Log.d("AOOO",topicName + " è il messaggio");
+
+        Boolean result = subscribeTopic(topicName);
+        Confirmation(result);
+        
+        Intent i=new Intent(this, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+
+
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
     }
 }
